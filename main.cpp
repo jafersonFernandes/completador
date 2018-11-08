d8202b2580ea1f123cacb12a18a79658c1beb1b7
jafersonFernandes <jaferson_fernandes@hotmail.com>
2018-11-08 09:40:46 -0300

    v0811

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <windows.h>
-#include <cstring>
 #include <string>
 
 using namespace std;
@@ -54,7 +53,7 @@ int add(PL **abb, string plN)
     else if (t == 1)
         add(&(*abb)->maior,plN);
     else
-        cout << "Palavra ja inserida" << endl;
+        (*abb)->cont++;
 }
 
 int  vPL(PL **abb, string op2, int x)
@@ -91,35 +90,70 @@ int main()
     pl = "abd";
     add(&abb,pl);
     int x = 0;
+    int y = 0;
+    int t = 0;
     char op;
     string op2;
+    string  frase;
+    frase.clear();
     while(true)
     {
         if(kbhit())
         {
+            p2:
             op = getch();
             if(op == 27)
                 break;
-            if(op != 8)
+            if(op != 8 && op != ' ')
             {
                 op2.push_back(op);
-                mgotoxy(0, 0);
+                p3:
                 system("cls");
-                cout << op2 << "\n";
+                cout << frase + op2 << "\n";
                 p1:
                 vPL(&abb, op2, x);
                 x++;
-                mgotoxy(x, 0);
+                y++;
+                mgotoxy(y, 0);
+                mgotoxy(10,10);
+                cout << x;
+                 mgotoxy(10,11);
+                cout << y;
             }
-            else if(op == 8 && x > 0)
+            else if(op == ' ')
+            {
+                frase = frase + op2 + ' ';
+                op2.clear();
+                x = 0;
+                y++;
+                goto p2;
+            }
+            else if(op == 8 && x !=0)
             {
                 x--;
+                y= y -2;
                 op2.resize(x);
-                mgotoxy(0,0);
                 system("cls");
-                cout << op2 << "\n";
+                cout << frase + op2 << "\n";
                 x--;
                 goto p1;
+            }
+            else if(op == 8 && x == 0)
+            {
+                t = frase.length()-1;
+                y = t;
+                while(frase[t-1] != ' ')
+                {
+                    t--;
+                }
+                while(t < frase.length())
+                {
+                    op2 = op2+frase[t];
+                    t++;
+                }
+                x = op2.length()-1;
+                frase.resize(frase.length()-x-1);
+                goto p3;
             }
             else
                 system("cls");
